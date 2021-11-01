fb8d8b10d5abc4ef1229d0ef594d83defc8f6215
SlimShady9 <slim.shady99q@gmail.com>
2021-11-01 16:29:27 -0500

    1

--- Backend/src/main/java/co/unbosque/mondsinc/controller/MdbSpringBootApplication.java
+++ Backend/src/main/java/co/unbosque/mondsinc/controller/MdbSpringBootApplication.java
@@ -3,9 +3,11 @@ package co.unbosque.mondsinc.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableAutoConfiguration
 @ComponentScan
 public class MdbSpringBootApplication {
--- Backend/src/main/java/co/unbosque/mondsinc/models/Documment.java
+++ Backend/src/main/java/co/unbosque/mondsinc/models/Documment.java
@@ -1,8 +1,6 @@
 package co.unbosque.mondsinc.models;
 
-import java.sql.Date;
 import java.util.List;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
