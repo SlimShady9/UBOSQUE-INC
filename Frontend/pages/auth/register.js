f1de8385ee914c809cf3455f5243d6d7fa8b8b39
Chiquillo <62142719+Laura-chiquillo@users.noreply.github.com>
2021-11-05 18:16:40 -0500

    validacion registro

--- Frontend/pages/auth/register.js
+++ Frontend/pages/auth/register.js
@@ -121,6 +121,7 @@ export default function Register() {
                     <input
                       type="password"
                       required
+                      name="confirmar"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Confirmar contraseña"
                     />
