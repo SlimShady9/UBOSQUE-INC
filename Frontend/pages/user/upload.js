59ce794a8d00720ea969903ce91368eb029c7282
Chiquillo <62142719+Laura-chiquillo@users.noreply.github.com>
2021-11-07 18:23:52 -0500

    vista de upload con validaciones

--- Frontend/pages/user/upload.js
+++ Frontend/pages/user/upload.js
@@ -1,11 +1,61 @@
 import React from "react";
-
+import Auth from "layouts/Auth.js";
 // layout for page
 
 export default function Upload() {
   return (
     <>
-      sube tu documento :3
+     <div className="container mx-auto px-4 h-full">
+        <div className="flex content-center items-center justify-center h-full">
+          <div className="w-full lg:w-6/12 px-4">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                <div className="text-blueGray-400 text-center mb-3 font-bold"></div>
+                  <h4>Subir documento</h4>
+              </div>
+              <form>
+                <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Tipo de solicitud
+                      </label>
+                      <input name = "solicitud" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,48}" required  type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                </div>
+
+                <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Número de referencia
+                    </label>
+                    <input name="referencia" pattern={ '((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])'}
+                    required  type="number" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                </div>
+
+                <div className="relative w-full mb-3">
+                  <input name="archivo"  type="file"  />
+                </div>
+
+                <div className="text-center mt-6">
+                    <button 
+                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="submit"
+                    >
+                      Subir
+                    </button>
+                </div>
+
+
+              </form>
+            </div> 
+          </div>
+        </div> 
+      </div>     
     </>
   );
 }
+
+Upload.layout = Auth;
