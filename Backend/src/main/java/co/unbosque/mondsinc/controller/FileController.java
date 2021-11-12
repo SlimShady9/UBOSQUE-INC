bee6dd6831380205c7de40f08b42627819a3ee5b
DGarnicaO19 <50021161+DGarnicaO19@users.noreply.github.com>
2021-11-12 15:38:09 -0500

    agrega lectura

--- Backend/src/main/java/co/unbosque/mondsinc/controller/FileController.java
+++ Backend/src/main/java/co/unbosque/mondsinc/controller/FileController.java
@@ -14,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.Iterator;
 
-
+import org.apache.poi.xssf.usermodel.*;
 @RestController
 @CrossOrigin(origins = {"https://localhost:3000", "https://mondsinc.vercel.app", "http://localhost:3000"})
 @RequestMapping("/api/v1/file")
@@ -44,5 +45,27 @@ public class FileController {
     throws IOException{
         
         XSSFWorkbook excel = new XSSFWorkbook(documento);
+        Iterator iterator=sheet.iterator();
+		
+		while(iterator.hasNext())
+		{
+			XSSFRow row=(XSSFRow) iterator.next();
+			
+			Iterator cellIterator=row.cellIterator();
+			
+			while(cellIterator.hasNext())
+			{
+				XSSFCell cell=(XSSFCell) cellIterator.next();
+				
+				switch(cell.getCellType())
+				{
+				case STRING: System.out.print(cell.getStringCellValue()); break;
+				case NUMERIC: System.out.print(cell.getNumericCellValue());break;
+				case BOOLEAN: System.out.print(cell.getBooleanCellValue()); break;
+				}
+				System.out.print(" |  ");
+			}
+			System.out.println();
+		}
     }
 }
