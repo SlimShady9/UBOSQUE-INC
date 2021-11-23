921b086c8e99efcdd3026f9a0a956036ecd223f4
Chiquillo <62142719+Laura-chiquillo@users.noreply.github.com>
2021-11-23 00:28:46 -0500

    intento tabla usuario

--- Frontend/components/Cards/UsersTable.js
+++ Frontend/components/Cards/UsersTable.js
@@ -1,11 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import { useEffect } from "react";
+import Swal from 'sweetalert2';
 // components
 
 import TableDropdown from "components/Dropdowns/TableDropdown.js";
 
 export default function UsersTable({ color }) {
+  var usuarios = []
+  useEffect(async () => {
+    const response = await fetch("http://localhost:8080/api/v1/users")
+
+    if (response.ok) {
+      usuarios = await response.json()
+      console.log(usuarios)
+    } else {
+      Swal.fire("Error", "Error en el servidor intentelo más tarde", "error")
+    }
+
+  })
+
   return (
     <>
       <div
@@ -94,64 +108,66 @@ export default function UsersTable({ color }) {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <h4
-                    className={
-                      "ml-3 font-bold" +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
-                    }
-                  >
-                    Argon Design System
-                  </h4>
-                </th>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  $2,500 USD
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <i className="fas fa-circle text-orange-500 mr-2"></i> pending
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div className="flex">
-                    <img
-                      src="/img/team-1-800x800.jpg"
-                      alt="..."
-                      className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow"
-                    ></img>
-                    <img
-                      src="/img/team-2-800x800.jpg"
-                      alt="..."
-                      className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
-                    ></img>
-                    <img
-                      src="/img/team-3-800x800.jpg"
-                      alt="..."
-                      className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
-                    ></img>
-                    <img
-                      src="/img/team-4-470x470.png"
-                      alt="..."
-                      className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
-                    ></img>
-                  </div>
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div className="flex items-center">
-                    <span className="mr-2">60%</span>
-                    <div className="relative w-full">
-                      <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                        <div
-                          style={{ width: "60%" }}
-                          className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
-                        ></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                  <TableDropdown />
-                </td>
-              </tr>
+              {usuarios.forEach(i =>(
+                                  <tr>
+                                  <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                    <h4
+                                      className={
+                                        "ml-3 font-bold" +
+                                        +(color === "light" ? "text-blueGray-600" : "text-white")
+                                      }
+                                    >
+                                      {i.nombre}
+                                    </h4>
+                                  </th>
+                                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                    $2,500 USD
+                                  </td>
+                                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                    <i className="fas fa-circle text-orange-500 mr-2"></i> pending
+                                  </td>
+                                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                    <div className="flex">
+                                      <img
+                                        src="/img/team-1-800x800.jpg"
+                                        alt="..."
+                                        className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow"
+                                      ></img>
+                                      <img
+                                        src="/img/team-2-800x800.jpg"
+                                        alt="..."
+                                        className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
+                                      ></img>
+                                      <img
+                                        src="/img/team-3-800x800.jpg"
+                                        alt="..."
+                                        className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
+                                      ></img>
+                                      <img
+                                        src="/img/team-4-470x470.png"
+                                        alt="..."
+                                        className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
+                                      ></img>
+                                    </div>
+                                  </td>
+                                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                    <div className="flex items-center">
+                                      <span className="mr-2">60%</span>
+                                      <div className="relative w-full">
+                                        <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
+                                          <div
+                                            style={{ width: "60%" }}
+                                            className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
+                                          ></div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </td>
+                                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                                    <TableDropdown />
+                                  </td>
+                                </tr>
+              ))}
             </tbody>
           </table>
         </div>
