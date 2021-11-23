1780c3d937ab9c16192c502db2cbaa9582ad1ff1
Chiquillo <62142719+Laura-chiquillo@users.noreply.github.com>
2021-11-23 13:36:58 -0500

    eliminar usuario( casi)

--- Frontend/components/Cards/UsersTable.js
+++ Frontend/components/Cards/UsersTable.js
@@ -142,7 +142,7 @@ export default function UsersTable({ color }) {
                     </div>
                   </td>
                   <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                    <TableDropdown />
+                    <TableDropdown id = {i.id}/>
                   </td>
                 </tr>
               ))}
